4584a2c540252393d8b40338f9285e934e5ba30c
panosz <panosz@gmail.com>
2021-05-14 14:35:42 +0300

    IMPRV. `Booz_xform` new method `calculate_modB_boozer`.

--- src/booz_xform/__init__.py
+++ src/booz_xform/__init__.py
@@ -1,2 +1,32 @@
-from ._booz_xform import Booz_xform
+import numpy as np
+from ._booz_xform import Booz_xform as Booz_xform_cpp
 from .plots import surfplot, symplot, modeplot, wireplot
+
+
+
+class Booz_xform(Booz_xform_cpp):
+
+    def calculate_modB_boozer(self, js, phi, theta):
+        """
+        Calculates :math:`|B|` on a surface in Boozer poloidal and toroidal angles.
+        Args:
+          js (int): The index among the output surfaces to plot.
+          phi (array-like): The toroidal angle values.
+          theta (array-like): The poloidal angle values.
+        """
+
+        phi = np.asanyarray(phi)
+        theta = np.asanyarray(theta)
+
+        modB = np.zeros_like(phi)
+
+        for jmn in range(len(self.xm_b)):
+            m = self.xm_b[jmn]
+            n = self.xn_b[jmn]
+            angle = m * theta - n * phi
+            modB += self.bmnc_b[jmn, js] * np.cos(angle)
+            if self.asym:
+                modB += self.bmns_b[jmn, js] * np.sin(angle)
+        return modB
+
+
