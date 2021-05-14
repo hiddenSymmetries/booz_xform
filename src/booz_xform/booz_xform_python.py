de86b46715fc3c7857b089dd09ce9e403e8d937a
panosz <panosz@gmail.com>
2021-05-14 20:20:29 +0300

    REF. booz_xform_python. factor out `_calculate_fourier_series`

--- src/booz_xform/booz_xform_python.py
+++ src/booz_xform/booz_xform_python.py
@@ -1,6 +1,23 @@
 import numpy as np
-
 from ._booz_xform import Booz_xform as Booz_xform_cpp
+
+
+def _calculate_fourier_series(m, n, cos_mn_ampl, sin_mn_ampl, phi, theta):
+
+    out = np.zeros_like(phi)
+
+    for jmn in range(len(m)):
+
+        m_i = m[jmn]
+        n_i = n[jmn]
+        angle = m_i * theta - n_i * phi
+
+        out += cos_mn_ampl[jmn] * np.cos(angle)
+        if not sin_mn_ampl.size == 0:
+            out += sin_mn_ampl[jmn] * np.sin(angle)
+
+    return out
+
 
 class Booz_xform(Booz_xform_cpp):
 
@@ -17,13 +34,16 @@ class Booz_xform(Booz_xform_cpp):
         phi = np.asanyarray(phi, dtype=np.float)
         theta = np.asanyarray(theta, dtype=np.float)
 
-        modB = np.zeros_like(phi)
+        if self.asym:
+            bmns_b = self.bmns[:, js]
+        else:
+            bmns_b = np.array([])
 
-        for jmn in range(len(self.xm_b)):
-            m = self.xm_b[jmn]
-            n = self.xn_b[jmn]
-            angle = m * theta - n * phi
-            modB += self.bmnc_b[jmn, js] * np.cos(angle)
-            if self.asym:
-                modB += self.bmns_b[jmn, js] * np.sin(angle)
-        return modB
+        return _calculate_fourier_series(self.xm_b,
+                                         self.xn_b,
+                                         self.bmnc_b[:, js],
+                                         bmns_b,
+                                         phi,
+                                         theta,
+                                         )
+
